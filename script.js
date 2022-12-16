ee70d59701d8fb060e7521e845357179c94c8a33
Artem <arttem.900@mail.ru>
2022-12-16 18:39:03 +0300

    prompt

--- script.js
+++ script.js
@@ -52,13 +52,76 @@ let inc = 10,
 //Практическое задание 1
 
 
-    //    let money = prompt("Ваш бюджет на месяц", '')
-    //     console.log(money);
+let money = +prompt("Ваш бюджет на месяц", '')
+let time = prompt("Введите чисо в формате YYYY-MM-DD", '')
 
-    //     let time = prompt("Введите чисо в формате YYYY-MM-DD", '')
-    //     console.log(time);
 
-          
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+}
+
+// for (let i = 0; i < 1; i++) {
+//     let a = prompt('Введите обязательную статью расходов в этом месяце', ' '),
+//         b = prompt('Во сколько обойдётся?', ' ')
+//     if (typeof (a) === 'string' && typeof (a) != null && typeof (b) != null && a != ' ' && b != ' ' && a.length < 50) {
+//         appData.expenses[a] = b
+//         console.log(appData.expenses);
+//     }else{
+//         i--
+//         console.log('Error');
+//     }
+// }
+
+// let i = 0
+    
+// while (i < 1) {
+//     let a = prompt('Введите обязательную статью расходов в этом месяце', ' '),
+//         b = prompt('Во сколько обойдётся?', ' ')
+//     if (typeof (a) === 'string' && typeof (a) != null && typeof (b) != null && a != ' ' && b != ' ' && a.length < 50) {
+//         appData.expenses[a] = b
+//         console.log(appData.expenses);
+//     }else{
+//         i--
+//         console.log('Error');
+//     }
+//     i++
+// }
+
+let i = 0
+do{
+    let a = prompt('Введите обязательную статью расходов в этом месяце', ' '),
+        b = prompt('Во сколько обойдётся?', ' ')  
+        if (typeof (a) === 'string' && typeof (a) != null && typeof (b) != null && a != ' ' && b != ' ' && a.length < 50) {
+            appData.expenses[a] = b
+            console.log(appData.expenses);
+        }else{
+            i--
+            console.log('Error');
+        }
+        i++
+}while(i < 1)
+
+appData.moneyPerDay = appData.budget / 30
+if(appData.moneyPerDay < 1000){
+    console.log('Минимальный уровень дохода');
+}else if(appData.moneyPerDay > 1000 && appData.moneyPerDay < 5000){
+    console.log('Средний уровень дохода');
+}else if(appData.moneyPerDay > 5000){
+    console.log('Высокий уровень дохода');
+}else{
+    console.log('Произошла ошибка');
+}
+console.log('Ваш дневной бюджет ' + Math.round(appData.moneyPerDay));
+
+// let budget = +prompt('Какой ваш месячный доход', " "),
+//     dohod = budget / 30
+// alert('Ваш дневной бюджет' + Math.round(dohod) + 'руб.')
+
 // function timeDate(){
 //     let time = +prompt('Введите чисо в формате YYYY-MM-DD',' ')
 //     let timePower = time
@@ -77,78 +140,68 @@ let inc = 10,
 //console.log(+prompt('sldhg','')); 
 
 
-// let appData = {
-//     budget: money,
-//     timeData: time,
-//     expenses: dfsb,
-//     optionalExpenses:fdg,
-//     income:[],
-//     savings: false
-// }
 
-// let budget = +prompt('Какой ваш месячный доход', " "),
-//     dohod = budget / 30
-//     alert('Ваш дневной бюджет' + Math.round(dohod) + 'руб.')
+
   
 // Условия
-    let o = 39;
-    if(o < 30){
-        console.log('Неверно');
-    }else if(o == 39){
-        console.log('Верно');
-    }else{
-        console.log('error');
-    }
-let y = 30
-    switch(y){
-        case y > 40:
-        console.log('Ne verno');
-        break;
-        case y != 30:
-            console.log('Verno');
-       break;
-       case 30:
-        console.log('----');
-        break; 
-        default:
-            console.log('&&&&');
-            break;
-    }
+//     let o = 39;
+//     if(o < 30){
+//         console.log('Неверно');
+//     }else if(o == 39){
+//         console.log('Верно');
+//     }else{
+//         console.log('error');
+//     }
+// let y = 30
+//     switch(y){
+//         case y > 40:
+//         console.log('Ne verno');
+//         break;
+//         case y != 30:
+//             console.log('Verno');
+//        break;
+//        case 30:
+//         console.log('----');
+//         break; 
+//         default:
+//             console.log('&&&&');
+//             break;
+//     }
     //Циклы
-    let numes = 10
-    while(numes < 20){
-        console.log(numes);
-        numes++
-    }
+    // let numes = 10
+    // while(numes < 20){
+    //     console.log(numes);
+    //     numes++
+    // }
 
-    do{
-        console.log(numes)
-        numes++
-    }while(numes < 25)
+    // do{
+    //     console.log(numes)
+    //     numes++
+    // }while(numes < 25)
     
 
-    for(i = 1; i < 10; i++){
-        if( i == 5){
-            continue
-        }
-        console.log(i);
-    }
-    for(i = 1; i < 10; i++){
-        if( i == 5){
-            break
-        }
-        console.log(i);
-    }
+    // for(i = 1; i < 10; i++){
+    //     if( i == 5){
+    //         continue
+    //     }
+    //     console.log(i);
+    // }
+    // for(i = 1; i < 10; i++){
+    //     if( i == 5){
+    //         break
+    //     }
+    //     console.log(i);
+    // }
 
 
     
 
 
-    for(i = 0; i < 40; i++){
-        if(i % 2 == 1){
-            continue
-        }
-        console.log(i);
-    }
+    // for(let i = 0; i < 10; i++){
+    //     if(i % 2 == 1){
+    //         continue
+    //     }
+    //     console.log(i);
+    // }
 
     
